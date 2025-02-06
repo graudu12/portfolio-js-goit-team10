0f07d0eccfa6d562cfca9ccc81bdd1286bfdf76b
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-02-06 02:10:02 +0200

    html/css

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,1 +1,18 @@
-
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Keyboard, Scrollbar], // Вказуємо потрібні модулі
+  slidesPerView: 1,
+  spaceBetween: 32,
+  loop: false, // Вимикаємо нескінченну прокрутку
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
