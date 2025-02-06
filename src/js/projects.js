66dddd692f9890c86431dbf50a5ad529b26f1f46
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-02-06 16:24:17 +0200

    css

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,18 +1,39 @@
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Scrollbar } from 'swiper/modules';
+
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Keyboard, Scrollbar], // Вказуємо потрібні модулі
-  slidesPerView: 1,
-  spaceBetween: 32,
-  loop: false, // Вимикаємо нескінченну прокрутку
+  modules: [Navigation, Keyboard, Scrollbar],
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  spaceBetween: 32,
+  slidesPerView: 'auto',
   keyboard: {
     enabled: true,
-    onlyInViewport: true,
+    pageUpDown: true,
   },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
+  loop: false,
 });
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Tab') {
+    event.preventDefault();
+
+    if (event.shiftKey) {
+      swiper.slidePrev();
+    } else {
+      swiper.slideNext();
+    }
+  }
+});
+
+document
+  .querySelector('.projects-button')
+  .addEventListener('click', function () {
+    window.open(
+      'https://github.com/graudu12/portfolio-js-goit-team10/',
+      '_blank'
+    );
+  });
