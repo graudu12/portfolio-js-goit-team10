729c264d1169a346ac08de8983a25a556df5fe65
Maryna <marin.naumch@gmail.com>
2025-02-09 01:10:44 +0200

    about

--- src/js/about.js
+++ src/js/about.js
@@ -1,3 +1,46 @@
+import Accordion from "accordion-js";
 
+import Swiper from 'swiper';
+import { Navigation, Keyboard  } from 'swiper/modules';
+import 'swiper/css';
+
+new Accordion(".accordion-container-about-me", {
+  elementClass: "ac-about-me",
+  triggerClass: "ac-trigger-about-me",
+  panelClass: "ac-panel-about-me",
+  activeClass: "is-active-about-me",
+  duration: 800,   
+  showMultiple: true,   
+  collapse: true,
+  openOnInit: [0],
+});
+
+ const swiper = new Swiper('.swiper-one', {
+   modules: [Navigation, Keyboard],
+   containerModifierClass: 'swiper-one',
+   slidesPerView: 2,
+    allowTouchMove: true,
+    loop: true,
+    preventClicks: true,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    navigation: {
+      nextEl: '.about-swiper-button-next',
+    },
+   breakpoints: {
+      320: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3
+    },
+    1440: {
+      slidesPerView: 6,
+    }
+  }
+ });
 
 
